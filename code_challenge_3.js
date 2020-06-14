58690034f006814fbaf174df194d64f31b47588c
Xintian Xu <xintianxu@Xintian-Xu.local>
2020-06-14 11:52:35 -0400

    initialize challenge 3

---
+++ code_challenge_3.js
@@ -0,0 +1,26 @@
+
+const color1;
+const color2;
+const
+
+function autoDetect(color){
+	if(color.length === 6 || color.length === 7){
+		if(color[i] === '#'){
+			color ===
+		}
+		return hexToRgb(color);
+
+	}
+	else {
+		return rgbToHex(color);
+	}
+}
+
+function hexToRgb(color){
+}
+
+function rgbToHex(color){
+
+}
+
+const convertedColor = autoDetect(color);
