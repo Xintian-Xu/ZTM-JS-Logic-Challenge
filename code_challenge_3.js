38b73958a4ace83e5eb56c7dadb25ab1040da364
Xintian Xu <xintianxu@Xintian-Xu.local>
2020-06-14 12:08:58 -0400

    2nd commit: first attempt of hexToRgb

--- code_challenge_3.js
+++ code_challenge_3.js
@@ -4,12 +4,8 @@ const color2;
 const
 
 function autoDetect(color){
-	if(color.length === 6 || color.length === 7){
-		if(color[i] === '#'){
-			color ===
-		}
+	if(color.length === 7){
 		return hexToRgb(color);
-
 	}
 	else {
 		return rgbToHex(color);
@@ -17,10 +13,18 @@ function autoDetect(color){
 }
 
 function hexToRgb(color){
+	let temp = color.substring(1);
+	temp.match(/..?/g);
+	const r = parseInt(temp[0], 16);
+	const g = parseInt(temp[1], 16);
+	const b = parseInt(temp[2], 16);
+
+	return `You entered ${color}, this color converted to RGB is (${r}, ${g}, ${b})`
+
 }
 
 function rgbToHex(color){
-
+	
 }
 
 const convertedColor = autoDetect(color);
